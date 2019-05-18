48e840426cd735ed30e7e1a94e4a0f4a5bfef8ae
EgorTrevelyan <egorepnikov@gmail.com>
2019-05-18 22:35:06 +0300

    Refactor Colossus subject

--- subjects/scala/colossus/src/main/scala/com/erepnikov/colossus/Server.scala
+++ subjects/scala/colossus/src/main/scala/com/erepnikov/colossus/Server.scala
@@ -2,8 +2,10 @@ package com.erepnikov.colossus
 
 import akka.actor.ActorSystem
 import colossus.core._
+import colossus.protocols.http.HttpMethod.Get
+import colossus.protocols.http.UrlParsing.{/, Root, on}
 import colossus.protocols.http._
-import colossus.service.Callback.Implicits._
+import colossus.service.Callback
 import colossus.service.GenRequestHandler.PartialHandler
 import com.github.plokhotnyuk.jsoniter_scala.core._
 import com.github.plokhotnyuk.jsoniter_scala.macros._
@@ -23,12 +25,14 @@ object Server extends App {
   }
 
   HttpServer.start("Colossus", 8080)(initContext => new Initializer(initContext) {
-    println("START")
     override def onConnect: RequestHandlerFactory = serverContext => new RequestHandler(serverContext) {
       override def handle: PartialHandler[Http] = {
-        case req if req.head.url == "/api/get-json-entity" => req.ok(Message(123, "Hello There", Array("And", "There")))
-        case req if req.head.url == "/api/get-plain-text" => req.ok("Hello There")
+        case req @ Get on Root / "api" / "get-json-entity" =>
+          Callback.successful(req.ok(Message(123, "Hello There", Array("And", "There"))))
+        case req @ Get on Root / "api" / "get-plain-text" =>
+          Callback.successful(req.ok("Hello There"))
       }
     }
   })
+  println("START")
 }
