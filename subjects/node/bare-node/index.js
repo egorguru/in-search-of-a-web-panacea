140b0c1afef85763f251b755b026e319828cb058
EgorTrevelyan <egorepnikov@gmail.com>
2019-07-16 04:54:21 +0300

    Update bare-node subject

--- subjects/node/bare-node/index.js
+++ subjects/node/bare-node/index.js
@@ -3,52 +3,57 @@ const url = require('url')
 
 const cluster = require('../cluster')
 
+const routes = {
+  '/api/get-tiny-json-entity': (_, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Hello There' }))
+  },
+  '/api/get-large-json-entity': (_, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({
+      id: 123,
+      message: 'Hello There',
+      entity: {
+        message: 'Hello There Again'
+      },
+      extra: [
+        'And',
+        'Again'
+      ]
+    }))
+  },
+  '/api/post-tiny-json-entity': (req, res) => {
+    let buffer1 = ''
+    req.on('data', (chunk) => buffer1 += chunk)
+    req.on('end', () => {
+      const body = JSON.parse(buffer1)
+      res.writeHead(201, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify(body))
+    })
+  },
+  '/api/post-large-json-entity': (req, res) => {
+    let buffer2 = ''
+    req.on('data', (chunk) => buffer2 += chunk)
+    req.on('end', () => {
+      const body = JSON.parse(buffer2)
+      res.writeHead(201, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify(body))
+    })
+  },
+  '/api/get-plain-text': (_, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('Hello There')
+  }
+}
+
 const server = http.createServer((req, res) => {
   const path = url.parse(req.url, true).pathname
-  switch (path) {
-    case '/api/get-tiny-json-entity':
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: 'Hello There' }))
-      break
-    case '/api/get-large-json-entity':
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({
-        id: 123,
-        message: 'Hello There',
-        entity: {
-          message: 'Hello There Again'
-        },
-        extra: [
-          'And',
-          'Again'
-        ]
-      }))
-      break
-    case '/api/post-tiny-json-entity':
-      let buffer1 = ''
-      req.on('data', (chunk) => buffer1 += chunk)
-      req.on('end', () => {
-        const body = JSON.parse(buffer1)
-        res.writeHead(201, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(body))
-      })
-      break
-    case '/api/post-large-json-entity':
-      let buffer2 = ''
-      req.on('data', (chunk) => buffer2 += chunk)
-      req.on('end', () => {
-        const body = JSON.parse(buffer2)
-        res.writeHead(201, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(body))
-      })
-      break
-    case '/api/get-plain-text':
-      res.writeHead(200, { 'Content-Type': 'text/plain' })
-      res.end('Hello There')
-      break
-    default:
-      res.writeHead(404, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: 'Not Found' }))
+  const handler = routes[path]
+  if (handler === undefined) {
+    res.writeHead(404, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Not Found' }))
+  } else {
+    handler(req, res)
   }
 })
 
