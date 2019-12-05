56a5e4baa39dd9a60413aba80b84f8f4b01109f0
EgorRepnikov <egorepnikov@gmail.com>
2019-12-05 15:07:24 +0300

    Update Dragonrend subject

--- subjects/node/dragonrend/index.js
+++ subjects/node/dragonrend/index.js
@@ -1,49 +1,38 @@
-const { dragonrend } = require('dragonrend')
+const { dragonrend, json, text } = require('dragonrend')
 
 const cluster = require('../cluster')
 
 const { GET, POST, START } = dragonrend({
+  noDelay: true,
   routing: {
     prefix: '/api'
   }
 })
 
-GET('/get-tiny-json-entity', ctx => {
-  ctx.response.json({ message: 'Hello There' })
-})
+GET('/get-tiny-json-entity', () => json({ message: 'Hello There' }))
 
-GET('/get-large-json-entity', ctx => {
-  ctx.response.json({
-    id: 123,
-    message: 'Hello There',
-    entity: {
-      message: 'Hello There Again'
-    },
-    extra: [
-      'And',
-      'Again'
-    ]
-  })
-})
+GET('/get-large-json-entity', () => json({
+  id: 123,
+  message: 'Hello There',
+  entity: {
+    message: 'Hello There Again'
+  },
+  extra: [
+    'And',
+    'Again'
+  ]
+}))
 
-POST('/post-tiny-json-entity', ctx => {
-  ctx.response.status(201).json(ctx.request.body)
-})
+POST('/post-tiny-json-entity', ctx => json(201, ctx.request.body))
 
-POST('/post-large-json-entity', ctx => {
-  ctx.response.status(201).json(ctx.request.body)
-})
+POST('/post-large-json-entity', ctx => json(201, ctx.request.body))
 
-GET('/get-plain-text', ctx => {
-  ctx.response.text('Hello There')
-})
+GET('/get-plain-text', () => text('Hello There'))
 
-GET('/get-tiny-json-entity-by-id/:id', ctx => {
-  ctx.response.json({
-    id: ctx.request.params.id,
-    message: 'Hello There'
-  })
-})
+GET('/get-tiny-json-entity-by-id/:id', ctx => json({
+  id: ctx.request.params.id,
+  message: 'Hello There'
+}))
 
 cluster(() => {
   START(8080, () => console.log('START'))
